647f6f525bab209cf7da96b914996e7c02ffd464
droidtvdroidtv2 <droidtvdroidtv2@gmail.com>
2026-08-28 14:51:13 +0700

    Update exec.c

--- fs/exec.c
+++ fs/exec.c
@@ -1695,7 +1695,7 @@ int do_execve(struct filename *filename,
 {
 	struct user_arg_ptr argv = { .ptr.native = __argv };
 	struct user_arg_ptr envp = { .ptr.native = __envp };
-ifdef CONFIG_KSU_MANUAL_HOOK
+#ifdef CONFIG_KSU_MANUAL_HOOK
 ksu_handle_execveat((int *)AT_FDCWD, &filename, &argv, &envp, 0);
 #endif
 	return do_execveat_common(AT_FDCWD, filename, argv, envp, 0);
